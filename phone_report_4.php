0a5a3413ce6fe6831e88541093891e16296bbc35
Neil Chintala <nchintala@scu.edu>
2014-05-10 08:00:52 -0700

    neil changes

---
+++ phone_report_4.php
@@ -0,0 +1,32 @@
+<?php
+    header('Content-type: text/xml');
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+ 
+    echo '<Response>';
+    session_start();
+    # @start snippet
+    $user_pushed = (int) $_REQUEST['Digits'];
+    $_SESSION['incidentType'] = $user_pushed;
+    # @end snippet
+    $testing = $_SESSION['test'];
+    if (($user_pushed >= 1) || ($user_pushed <=9))
+    {
+        ?>
+
+        
+        <Gather action="<?php echo 'http://' . dirname($_SERVER["SERVER_NAME"] .  $_SERVER['PHP_SELF']) . '/phone_report_5.php'; ?>" method='GET' numDigits="1">
+        <?php
+        echo '<Say>Where did the incident take place?</Say>';
+        echo '<Say>If it was nearby, press 1.</Say>';
+        echo '<Say>To manually enter your postal code, press 2.</Say>';
+        echo '</Gather>';
+    }
+    else {
+        // We'll implement the rest of the functionality in the 
+        // following sections.
+        echo "<Say>Sorry, I can't do that yet.</Say>";
+        echo '<Redirect>handle-incoming-call.php</Redirect>';
+    }
+ 
+    echo '</Response>';
+?>
